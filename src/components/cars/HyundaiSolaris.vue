8b3e56c9a3c5c69b7a9fc1ef98f0f072f3b63876
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-04-07 16:08:50 +0700

    Initial commit

---
+++ src/components/cars/HyundaiSolaris.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="car">
+    <h6>{{title}}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title: 'HyundaiSolaris'
+    }
+  },
+name: "HyundaiSolaris"
+}
+</script>
+
+<style lang="scss" scoped>
+.car {
+  background-color: $green-6;
+}
+</style>
---
+++ src/components/cars/SkodaOctavia.vue
@@ -0,0 +1,13 @@
+<template>
+<h6>Skoda Octavia</h6>
+</template>
+
+<script>
+export default {
+name: "SkodaOctavia"
+}
+</script>
+
+<style scoped>
+
+</style>
